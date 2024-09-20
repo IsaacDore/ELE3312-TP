9d226683eb79fda4414fb0784eb1059134e76f64
IsaacDorePolymtl <isaac.dore@polymtl.ca>
2024-09-20 13:03:58 -0400

    adding missing commas

--- TP3/enonce3/Src/positive.asm
+++ TP3/enonce3/Src/positive.asm
@@ -2,10 +2,10 @@
         AREA |.text|, CODE, READONLY
         EXPORT positive
 poitive PROC
-        LDR r0 [r0]    ; r0 = *value
-        LSR r1 r0 #31   ; r1 = y
-        EOR r0 r1       ; r0 = *value^y
-        SUB r0 r1       ; r0 = (*value^y)-y
+        LDR r0, [r0]    ; r0 = *value
+        LSR r1, r0 #31   ; r1 = y
+        EOR r0, r1       ; r0 = *value^y
+        SUB r0, r1       ; r0 = (*value^y)-y
         BX LR
         ENDP
         END
