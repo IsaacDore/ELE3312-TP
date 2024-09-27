6a05d9b462910af2038c8e5f8e16031bee07ae59
IsaacDore <76712828+IsaacDore@users.noreply.github.com>
2024-09-27 10:45:11 -0400

    Tp4 (#2)
    * copy mytest1 from tp2 and regenerate code
    
    * fct_count
    
    * experience 1 complete
    
    * experience 1 complete
    
    * exp 2
    
    * use microlib
    
    * build.md
    
    * added usart2 drivers
    
    * test app
    
    * test function
    
    * optimization 0
    
    * TP3
    
    * add function to prokject
    
    * adding missing commas
    
    * fix warnings
    
    * done
    
    * removed unsused files
    
    * remove useless while loop
    
    * Initialization TP4
    
    * stm32cubemx code gen
    
    * gitkeep
    
    * added keyboard read functions
    
    * chose compiler version
    
    * .gitignore
    
    * redirect stdout
    
    * test code
    
    * clang-format on main
    
    * get button
    
    * fix mistakes
    
    * enable usart2
    
    * redo prelab
    
    * add ili341
    
    * hello world
    
    * added rcc
    
    * so it works only in debug
    
    * enonce 2
    
    * keytoken
    
    * irq handler
    
    * y da fok
    
    * pull-up u fuk
    
    * finsidhed
    
    * finished
    
    * en-tete
    
    ---------
    
    Co-authored-by: IsaacDorePolymtl <isaac.dore@polymtl.ca>

---
+++ TP4/Enonce2/Core/Src/main.c
@@ -0,0 +1,320 @@
+/*
+  laboratoire = 4
+  groupe = 2
+  date = 2024/septembre/27
+  auteurs = Isaac Doré, Arnaud Gagner, Justin Botbol
+  équipe = 05
+*/
+
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "dma.h"
+#include "gpio.h"
+#include "spi.h"
+#include "usart.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdio.h"
+
+#include "ili9341.h"
+#include "ili9341_gfx.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+ili9341_t *_screen;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void selectRow(int r) {
+  if (r == 0)
+    HAL_GPIO_WritePin(R1_GPIO_Port, R1_Pin, GPIO_PIN_RESET);
+  else
+    HAL_GPIO_WritePin(R1_GPIO_Port, R1_Pin, GPIO_PIN_SET);
+  if (r == 1)
+    HAL_GPIO_WritePin(R2_GPIO_Port, R2_Pin, GPIO_PIN_RESET);
+  else
+    HAL_GPIO_WritePin(R2_GPIO_Port, R2_Pin, GPIO_PIN_SET);
+  if (r == 2)
+    HAL_GPIO_WritePin(R3_GPIO_Port, R3_Pin, GPIO_PIN_RESET);
+  else
+    HAL_GPIO_WritePin(R3_GPIO_Port, R3_Pin, GPIO_PIN_SET);
+  if (r == 3)
+    HAL_GPIO_WritePin(R4_GPIO_Port, R4_Pin, GPIO_PIN_RESET);
+  else
+    HAL_GPIO_WritePin(R4_GPIO_Port, R4_Pin, GPIO_PIN_SET);
+}
+
+char readCol() {
+  int result = 0;
+  if (HAL_GPIO_ReadPin(C1_GPIO_Port, C1_Pin) == GPIO_PIN_RESET)
+    result += 1;
+  if (HAL_GPIO_ReadPin(C2_GPIO_Port, C2_Pin) == GPIO_PIN_RESET)
+    result += 2;
+  if (HAL_GPIO_ReadPin(C3_GPIO_Port, C3_Pin) == GPIO_PIN_RESET)
+    result += 4;
+  if (HAL_GPIO_ReadPin(C4_GPIO_Port, C4_Pin) == GPIO_PIN_RESET)
+    result += 8;
+  return result;
+}
+
+int fputc(int ch, FILE *f) {
+  HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
+  return ch;
+}
+
+int row = 0;
+volatile int hour = 0;
+volatile int min = 0;
+volatile int sec = 0;
+int milli = 0;
+volatile int token = 1;
+volatile int keytoken = 0;
+
+// every ms
+void HAL_SYSTICK_Callback(void) {
+  milli++;
+
+  selectRow(row = ((row + 1) % 4));
+  int col = readCol();
+  keytoken |= (col << (row * 4));
+
+  if (milli < 1000) {
+    return;
+  }
+  milli = 0;
+  sec++;
+  token = 1;
+  if (sec < 60) {
+    return;
+  }
+  sec = 0;
+  min++;
+  if (min < 60) {
+    return;
+  }
+  min = 0;
+  hour++;
+  if (hour < 24) {
+    return;
+  }
+  hour = 0;
+  return;
+}
+/* USER CODE END 0 */
+
+/**
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
+   */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_SPI1_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  _screen = ili9341_new(&hspi1, Void_Display_Reset_GPIO_Port,
+                        Void_Display_Reset_Pin, TFT_CS_GPIO_Port, TFT_CS_Pin,
+                        TFT_DC_GPIO_Port, TFT_DC_Pin, isoLandscape, NULL, NULL,
+                        NULL, NULL, itsNotSupported, itnNormalized);
+  ili9341_fill_screen(_screen, ILI9341_BLACK);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1) {
+    /* USER CODE END WHILE */
+    while (token == 0)
+      ;
+    if (keytoken & (1 << 0)) {
+      // printf("inc sec\n");
+      sec++;
+      if (sec >= 60) {
+        sec = 0;
+      }
+    }
+    if (keytoken & (1 << 1)) {
+      // printf("dec sec\n");
+      sec -= 2;
+      if (sec < 0) {
+        sec = 0;
+      }
+    }
+    if (keytoken & (1 << 2)) {
+      // printf("inc min\n");
+      min++;
+      if (min >= 60) {
+        min = 0;
+      }
+    }
+    if (keytoken & (1 << 3)) {
+      // printf("dec min\n");
+      min--;
+      if (min < 0) {
+        min = 0;
+      }
+    }
+    if (keytoken & (1 << 4)) {
+      // printf("inc hour\n");
+      hour++;
+      if (hour >= 25) {
+        hour = 0;
+      }
+    }
+    if (keytoken & (1 << 5)) {
+      // printf("dec hour\n");
+      hour--;
+      if (hour < 0) {
+        hour = 0;
+      }
+    }
+    char buffer[20] = {0};
+    sprintf(buffer, "%2i:%2i:%2i", hour, min, sec);
+    ili9341_fill_screen(_screen, ILI9341_BLACK);
+    ili9341_text_attr_t time_attr = {&ili9341_font_11x18, ILI9341_WHITE,
+                                     ILI9341_BLACK, 0, 0};
+    ili9341_draw_string(_screen, time_attr, buffer);
+    keytoken = 0;
+    token = 0;
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+   */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+   * in the RCC_OscInitTypeDef structure.
+   */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1) {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef USE_FULL_ASSERT
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line
+     number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ TP4/prelab/Core/Src/main.c
@@ -0,0 +1,313 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "dma.h"
+#include "gpio.h"
+#include "spi.h"
+#include "usart.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdio.h"
+
+#include "ili9341.h"
+#include "ili9341_gfx.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+enum Button {
+  B1 = 0,
+  B2,
+  B3,
+  BA,
+  B4,
+  B5,
+  B6,
+  BB,
+  B7,
+  B8,
+  B9,
+  BC,
+  BSTAR,
+  B0,
+  BDASH,
+  BD
+};
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+ili9341_t *_screen;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void selectRow(int r) {
+  if (r == 1)
+    HAL_GPIO_WritePin(R1_GPIO_Port, R1_Pin, GPIO_PIN_RESET);
+  else
+    HAL_GPIO_WritePin(R1_GPIO_Port, R1_Pin, GPIO_PIN_SET);
+  if (r == 2)
+    HAL_GPIO_WritePin(R2_GPIO_Port, R2_Pin, GPIO_PIN_RESET);
+  else
+    HAL_GPIO_WritePin(R2_GPIO_Port, R2_Pin, GPIO_PIN_SET);
+  if (r == 3)
+    HAL_GPIO_WritePin(R3_GPIO_Port, R3_Pin, GPIO_PIN_RESET);
+  else
+    HAL_GPIO_WritePin(R3_GPIO_Port, R3_Pin, GPIO_PIN_SET);
+  if (r == 4)
+    HAL_GPIO_WritePin(R4_GPIO_Port, R4_Pin, GPIO_PIN_RESET);
+  else
+    HAL_GPIO_WritePin(R4_GPIO_Port, R4_Pin, GPIO_PIN_SET);
+}
+
+char readCol() {
+  int result = 0;
+  if (HAL_GPIO_ReadPin(C1_GPIO_Port, C1_Pin) == GPIO_PIN_RESET)
+    result += 1;
+  if (HAL_GPIO_ReadPin(C2_GPIO_Port, C2_Pin) == GPIO_PIN_RESET)
+    result += 4;
+  if (HAL_GPIO_ReadPin(C3_GPIO_Port, C3_Pin) == GPIO_PIN_RESET)
+    result += 16;
+  if (HAL_GPIO_ReadPin(C4_GPIO_Port, C4_Pin) == GPIO_PIN_RESET)
+    result += 64;
+  return result;
+}
+
+unsigned int poll_keyboard_state(unsigned int prev_state) {
+  prev_state &= 0x55555555;
+  prev_state <<= 1;
+  unsigned int state = 0;
+  for (int i = 4; i != 0; i--) {
+    selectRow(4);
+    HAL_Delay(10);
+    state <<= 8;
+    state |= readCol();
+  }
+  return state | prev_state;
+}
+
+char get_button(enum Button b, unsigned int k_state) {
+  unsigned int mask = 1;
+  mask <<= (b * 2);
+  return ((k_state & mask) != 0);
+}
+
+char get_button_held(enum Button b, unsigned int k_state) {
+  unsigned int mask = 0b11;
+  mask <<= (b * 2);
+  return ((k_state & mask) != 0);
+}
+
+char get_button_up(enum Button b, unsigned int k_state) {
+  k_state ^= 0x55555555;
+  unsigned int mask = 0b10;
+  mask <<= (b * 2);
+  return ((k_state & mask) != 0);
+}
+
+char get_button_down(enum Button b, unsigned int k_state) {
+  k_state ^= 0xAAAAAAAA;
+  unsigned int mask = 1;
+  mask <<= (b * 2);
+  return ((k_state & mask) != 0);
+}
+
+int fputc(int ch, FILE *f) {
+  HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
+  return ch;
+}
+
+volatile int hour = 0;
+volatile int min = 0;
+volatile int sec = 0;
+volatile int milli = 0;
+volatile int token = 1;
+
+// every ms
+void HAL_SYSTICK_Callback(void) {
+  milli++;
+  if (milli < 1000)
+    return;
+  milli = 0;
+  sec++;
+  token = 1;
+  if (sec < 60)
+    return;
+  sec = 0;
+  min++;
+  if (min < 60)
+    return;
+  min = 0;
+  hour++;
+  if (hour < 24)
+    return;
+  hour = 0;
+  return;
+}
+/* USER CODE END 0 */
+
+/**
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
+   */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_SPI1_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  _screen = ili9341_new(&hspi1, Void_Display_Reset_GPIO_Port,
+                        Void_Display_Reset_Pin, TFT_CS_GPIO_Port, TFT_CS_Pin,
+                        TFT_DC_GPIO_Port, TFT_DC_Pin, isoLandscape, NULL, NULL,
+                        NULL, NULL, itsNotSupported, itnNormalized);
+  ili9341_fill_screen(_screen, ILI9341_BLACK);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1) {
+    /* USER CODE END WHILE */
+    while (!token)
+      ;
+    token = 0;
+    char buffer[20] = {0};
+    sprintf(buffer, "%2i:%2i:%2i", hour, min, sec);
+    ili9341_fill_screen(_screen, ILI9341_BLACK);
+    ili9341_text_attr_t time_attr = {&ili9341_font_11x18, ILI9341_WHITE,
+                                     ILI9341_BLACK, 0, 0};
+    ili9341_draw_string(_screen, time_attr, buffer);
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+   */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+   * in the RCC_OscInitTypeDef structure.
+   */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1) {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef USE_FULL_ASSERT
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line
+     number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
